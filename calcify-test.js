820b3abe67848eaafd71b055f84a8ae7478c77fe
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-27 20:27:49 +0800

    Updated test.

--- calcify-test.js
+++ calcify-test.js
@@ -2,6 +2,6 @@
 const assert = require( "assert" );
 const calcify = require( "./calcify.js" );
 
-assert.equal( typeof calcify( { "hello": "world" } ), "string", "should return 'string'" );
+assert.equal( typeof calcify( { "hello": "world" } ), "string", "should be equal to 'string'" );
 
 console.log( "ok" );
